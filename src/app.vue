76059be3e76af2cbdaf452d0d32d21eaefffb7b4
153913232a <1349788562@qq.com>
2018-12-27 18:35:44 +0800

    first

---
+++ src/app.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <img src="./images/logo.png" alt="logo" />
+        <span>{{msg}}</span>
+        <p class="test">test text</p>
+    </div>
+</template>
+<script>
+    import test from 'common/test'
+    export default {
+        name:'app',
+        data(){
+            return {
+                msg:"hello vue !!"
+            }
+        },
+        created() {
+            console.log(test())
+        }
+    }
+</script>
+<style >
+    .test{
+        color:#020202
+    }
+</style>
+<style lang="less">
+    @import "styles/test";
+    #app{
+        img{width:100px;}
+    }
+</style>
